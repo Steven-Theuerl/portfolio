27b34fddab77affccda637e8d94b00a1bef3c08b
unknown <augustus.winters@gmail.com>
2024-05-17 19:36:03 -0400

    PCNavbar and PCTop components roughly finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
-function App() {
+import NavbarPC from './Components/Navbar/NavbarPC'
+import TopPC from './Components/Top/TopPC'
+
+
+const App = () => {
+  const isPC = useMediaQuery({query: '(min-width: 376px)'})
+  const isMobile = useMediaQuery({ query: '(max-width: 375px)' })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {isPC && <NavbarPC/>}
+      {isPC && <TopPC/>}
+      {isMobile && <p>You are a tablet or mobile phone</p>}
+    </>
+  )
 }
-
 export default App;
---
+++ src/Components/Navbar/NavbarPC.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './NavbarPC.module.css'
+
+const NavbarPC = () => {
+  return (
+    <div className={styles.entireNavbar}>
+        <div className={styles.navContainer}>
+            <div className={styles.navSignature}>
+                Steven Theuerl
+            </div>
+            <div className={styles.navULContainer}>
+                <ul className={styles.navUL}>
+                    <li className={styles.navULliItem}>About</li>
+                    <li className={styles.navULliItem}>Work</li>
+                    <li className={styles.navULliItem}>
+                        <button className={styles.navULliItemTalkBox}>Talk with me</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavbarPC
