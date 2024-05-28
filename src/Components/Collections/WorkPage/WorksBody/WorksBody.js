78293270ce344b7b2fcb5418d6220ffeb3ca3f94
unknown <augustus.winters@gmail.com>
2024-05-28 13:12:44 -0400

    Checkpoint before adding react Carousel

---
+++ src/Components/Collections/WorkPage/WorksBody/WorksBody.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './WorksBody.module.css'
+
+const WorksBody = () => {
+  return (
+    <div className={styles.entireWorksBody}>
+        <div className={styles.worksBodyContainer}>
+            Hey
+        </div>
+    </div>
+  )
+}
+
+export default WorksBody
--- src/Components/Groups/WorkGroup.js
+++ src/Components/Groups/WorkGroup.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../Collections/HomePage/Navbar/Navbar'
 import WorkHero from '../Collections/WorkPage/WorkHero/WorkHero'
 import WorksHeader from '../Collections/WorkPage/WorksHeader/WorksHeader'
-
+import WorksBody from '../Collections/WorkPage/WorksBody/WorksBody'
 
 const WorkGroup = () => {
   return (
@@ -10,6 +10,7 @@ const WorkGroup = () => {
         <Navbar/>
         <WorkHero/>
         <WorksHeader/>
+        <WorksBody/>
     </>
   )
 }
